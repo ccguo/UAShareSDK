e2e20a58b80375698e94ea47d6f9a6723b58096c
上海－郭朝阳 <guochaoyang@jd.com>
2015-12-03 17:12:40 +0800

    Initial Commit

---
+++ UAShareSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UAShareSDK
+//
+//  Created by guochaoyang on 15/12/3.
+//  Copyright © 2015年 guochaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UAShareSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UAShareSDK
+//
+//  Created by guochaoyang on 15/12/3.
+//  Copyright © 2015年 guochaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
